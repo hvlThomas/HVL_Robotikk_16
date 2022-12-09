2fdee8d06931bb495c23a74954157f35974b6cc7
Robotikk_16 <120093026+hvlThomas@users.noreply.github.com>
2022-12-09 10:30:44 +0100

    Update rrtproj.m
    Signed-off-by: Robotikk_16 <120093026+hvlThomas@users.noreply.github.com>

--- Vedlegg-semesteroppgave/MATLAB-code/rrtproj.m
+++ Vedlegg-semesteroppgave/MATLAB-code/rrtproj.m
@@ -1,3 +1,7 @@
+%Enkelt script for å teste peter corke's toolbox RRT
+%Bruker rrtmap filen
+%Viser at RRT-objektet ignorerer hindringer i kartet, da det plottes i 3 dimensjoner istedet for 2
+
 robot = Bicycle('steermax', 0.5);
 rrtnav = RRT(robot, rrtmap, 'npoints', 2500, 'root', [10 10 0], 'simtime', 4);
 rrtnav.plan();
